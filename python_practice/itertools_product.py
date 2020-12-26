5486c81a518e5d6b1830cc13fc23d15885b05277
Tea Silagava <tea.silagava@gmail.com>
2020-12-26 14:25:25 +0400

    changes in itertools_product.py

--- python_practice/itertools_product.py
+++ python_practice/itertools_product.py
@@ -6,8 +6,5 @@ B = input()
 A_new = [int(i) for i in A.split()]
 B_new = [int(i) for i in B.split()]
 
-<<<<<<< HEAD
 print(*product(A_new, B_new))
-=======
 print(*product(A_new, B_new))
->>>>>>> 8fae99531e62efa07f4452e2797d990275a17223
