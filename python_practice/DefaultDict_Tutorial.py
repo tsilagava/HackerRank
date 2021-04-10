d2a94359e1866eccf8609e7684eb90a1c6b26303
Tea Silagava <34042338+tsilagava@users.noreply.github.com>
2021-04-11 02:12:05 +0400

    Update DefaultDict_Tutorial.py

--- python_practice/DefaultDict_Tutorial.py
+++ python_practice/DefaultDict_Tutorial.py
@@ -4,11 +4,14 @@ first_list = ['a', 'a', 'b', 'a', 'b']
 second_list = ['a', 'b', 'c']
 
 from collections import defaultdict
-
 d = defaultdict(list)
 n, m = map(int, input().split())
 for i in range(n):
-    d[input()].append(i + 1)
-
+    d[input()].append(i+1)
+    
 for j in range(m):
-    print(*d[input()] or [-1])
+    a = input()
+    if a in d.keys():
+        print(*d[a])
+    else:
+        print(-1)
